4979c25c9ae5a9b4d7cdb7ef66cdc4b508bd9085
otto-menza <otoniel.jump@gmail.com>
2026-08-05 17:56:55 -0500

    Create components

---
+++ src/app/componentes/cabecero/cabecero.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecero',
+  imports: [],
+  templateUrl: './cabecero.html',
+  styleUrl: './cabecero.css',
+})
+export class Cabecero {
+
+}
