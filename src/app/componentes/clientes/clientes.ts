0f8769936fc30fc0014552d39c3f5de7483b20e4
otto-menza <otoniel.jump@gmail.com>
2026-08-11 16:10:00 -0500

    configuration Clientes component

--- src/app/componentes/clientes/clientes.ts
+++ src/app/componentes/clientes/clientes.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { Cliente } from '../../modelo/cliente.modelo';
+import { ClienteService } from '../../servicios/cliente';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-clientes',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './clientes.html',
   styleUrl: './clientes.css',
 })
 export class Clientes {
+  clientes: Cliente[] | null = null;
 
+  constructor(private clientesServicio: ClienteService){}
+
+  ngOnInit(){
+    this.clientesServicio.getClientes().subscribe(clientes => {
+      this.clientes = clientes;
+    });
+  }
 }
