6a498aefbc886b9a3508b29452df65e100500fc9
otto-menza <otoniel.jump@gmail.com>
2026-08-10 22:00:52 -0500

    configurtion of firestore module. providers en main.ts

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDWxDO9qrACECESd7u0NGAngBzxIuu8zPM",
+        authDomain: "control-clientes-6b34d.firebaseapp.com",
+        projectId: "control-clientes-6b34d",
+        storageBucket: "control-clientes-6b34d.firebasestorage.app",
+        messagingSenderId: "78269411017",
+        appId: "1:78269411017:web:539ef74ccdc5cfb7182718"
+    }
+};
