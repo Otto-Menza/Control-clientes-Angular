1b626dabbfcaab36ead4460b172f55be4af2e917
otto-menza <otoniel.jump@gmail.com>
2026-08-14 15:34:15 -0500

    configuration and fix bug about the version firabase

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Cabecero } from "./componentes/cabecero/cabecero";
 import { PiePagina } from "./componentes/pie-pagina/pie-pagina";
+import { Clientes } from "./componentes/clientes/clientes";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Cabecero, PiePagina],
+  imports: [RouterOutlet, Cabecero, PiePagina, Clientes],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
