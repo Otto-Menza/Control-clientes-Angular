9fb634ddc7c9bfcd71ae1357fc3e85ca03099c11
otto-menza <otoniel.jump@gmail.com>
2026-08-18 10:55:05 -0500

    Add Login-guardian how controlled of routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { Tablero } from './componentes/tablero/tablero';
 import { Login } from './componentes/login/login';
 import { EditarCliente } from './componentes/editar-cliente/editar-cliente';
 import { NoEncontrado } from './componentes/no-encontrado/no-encontrado';
+import { LoginGuardianService } from './servicios/login-guardian';
 
 export const routes: Routes = [
-    {path: '', component: Tablero},
+    {path: '', component: Tablero, canActivate: [LoginGuardianService]},
     {path: 'login', component: Login},
-    {path: 'cliente/editar/:id', component: EditarCliente},
+    {path: 'cliente/editar/:id', component: EditarCliente, canActivate: [LoginGuardianService]},
     {path: '**', component: NoEncontrado}
 ];
